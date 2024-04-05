3fd99e00630bb4bcd1aa080a1be9f663f58a17e9
M P Darshan <127178559+DarshanD4@users.noreply.github.com>
2024-04-05 14:37:35 +0530

    linear.py

--- linear.py
+++ linear.py
@@ -1,1 +1,7 @@
-import numpy
+import numpy as np
+c = 16
+m = 16
+x = [x for x in range(1, 11)]  # Adjusted range to start from 1
+for i in range(len(x)):
+    result = c * x[i]
+    print(f"{c} * {x[i]} = {result}")
